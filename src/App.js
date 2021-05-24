5c82b260000465116c401c87c27a46c9bbd16bf2
Noof <noof.mohammed998@gmail.com>
2021-05-24 12:48:17 +0300

    Q3 and Q4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState }  from 'react';
 
 // jsx
 const App = () => {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([{userId:3, id:"200", title:"cook", body: "how cook" },{userId:"4", id:"300", title:"play", body: "how play" }])
   return (
     <>
     <div>
