826c93b05bdc8016955f81b8abf4eb9ef93d2794
Noof <noof.mohammed998@gmail.com>
2021-05-24 12:33:05 +0300

    Q1 and Q2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState }  from 'react';
+
+
+// jsx
+const App = () => {
+  const [posts, setPosts] = useState([])
+  return (
+    <>
+    <div>
+  <h1>Blog App</h1>
+  </div>
+  </>
+  );
+};
+
+export default App 
